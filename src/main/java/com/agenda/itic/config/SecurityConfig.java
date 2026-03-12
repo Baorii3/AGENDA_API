e07252b73b9e0505316f0c1407214683df539a38
Ian <baorii310@gmail.com>
2026-03-12 17:09:48 +0100

    cambios

--- src/main/java/com/agenda/itic/config/SecurityConfig.java
+++ src/main/java/com/agenda/itic/config/SecurityConfig.java
@@ -1,13 +1,12 @@
 package com.agenda.itic.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,6 +20,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/activitats/activitats").permitAll()
                         .requestMatchers(HttpMethod.GET, "/activitats/activitat/**").permitAll()
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
+                    .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
                 .oauth2Login(oauth -> oauth
                         .defaultSuccessUrl("/oauth/google/home", true));
         return http.build();
