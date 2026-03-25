b7b8276fa147e567b3d6d55a2c0fd67cd8ec24dd
Ian <baorii310@gmail.com>
2026-03-25 13:35:51 +0100

    quito import sobrante

--- src/main/java/com/agenda/itic/controller/CorreoPermitidoController.java
+++ src/main/java/com/agenda/itic/controller/CorreoPermitidoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.agenda.itic.service.CorreoPermitidoService;
 import com.agenda.itic.model.CorreoPermitido;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/correos-permitidos")
