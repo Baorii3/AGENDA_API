e126bdf94f5594bf48ea5f7d4c57a86bde695233
Ian <baorii310@gmail.com>
2026-05-17 16:37:30 +0200

    filterChain

--- src/main/java/com/agenda/itic/config/SecurityConfig.java
+++ src/main/java/com/agenda/itic/config/SecurityConfig.java
@@ -22,79 +22,83 @@ import java.util.List;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    @Value("${aws.cognito.region}")
-    private String cognitoRegion;
+        @Value("${aws.cognito.region}")
+        private String cognitoRegion;
 
-    @Value("${aws.cognito.userPoolId}")
-    private String cognitoUserPoolId;
+        @Value("${aws.cognito.userPoolId}")
+        private String cognitoUserPoolId;
 
-    @Bean
-    @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "local", matchIfMissing = false)
-    public SecurityFilterChain filterChainLocal(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/activitats/**").permitAll()
-                        .requestMatchers("/salas/**").permitAll()
-                        .requestMatchers("/admin-permitidos/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/usuaris/token").authenticated()
-                        .requestMatchers("/usuaris/**").permitAll()
-                        .requestMatchers("/dispositius/**").permitAll()
-                        .requestMatchers("/roles/**").permitAll()
-                        .requestMatchers("/recursos/**").permitAll()
-                        .requestMatchers("/permisos/**").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/swagger-ui.html").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .anyRequest().permitAll());
-        return http.build();
-    }
+        @Bean
+        @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "local", matchIfMissing = false)
+        public SecurityFilterChain filterChainLocal(HttpSecurity http) throws Exception {
+                http
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                                .csrf(csrf -> csrf.disable())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/activitats/**").permitAll()
+                                                .requestMatchers("/salas/**").permitAll()
+                                                .requestMatchers("/admin-permitidos/**").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/usuaris/token").authenticated()
+                                                .requestMatchers("/usuaris/**").permitAll()
+                                                .requestMatchers("/dispositius/**").permitAll()
+                                                .requestMatchers("/roles/**").permitAll()
+                                                .requestMatchers("/recursos/**").permitAll()
+                                                .requestMatchers("/permisos/**").permitAll()
+                                                .requestMatchers("/swagger-ui/**").permitAll()
+                                                .requestMatchers("/swagger-ui.html").permitAll()
+                                                .requestMatchers("/v3/api-docs/**").permitAll()
+                                                .anyRequest().permitAll());
+                return http.build();
+        }
 
-    @Bean
-    @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "prod")
-    public SecurityFilterChain filterChainProd(HttpSecurity http, CustomOAuth2UserService customOAuth2UserService)
-            throws Exception {
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/activitats/**").permitAll()
-                        .requestMatchers("/salas/**").permitAll()
-                        .requestMatchers("/admin-permitidos/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/usuaris/token").permitAll()
-                        .requestMatchers("/usuaris/**").authenticated()
-                        .requestMatchers("/dispositius/**").permitAll()
-                        .requestMatchers("/roles/**").permitAll()
-                        .requestMatchers("/recursos/**").permitAll()
-                        .requestMatchers("/permisos/**").permitAll()
-                        .requestMatchers("/oauth2/**").permitAll()
-                        .requestMatchers("/login/**").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/swagger-ui.html").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .anyRequest().permitAll())
-                .oauth2ResourceServer(
-                        oauth2 -> oauth2.jwt(org.springframework.security.config.Customizer.withDefaults()))
-                .oauth2Login(oauth -> oauth
-                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                        .defaultSuccessUrl("/oauth/google/home", true)
-                        .failureUrl("/login.html?error=unauthorized"));
-        return http.build();
-    }
+        @Bean
+        @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "prod")
+        public SecurityFilterChain filterChainProd(HttpSecurity http, CustomOAuth2UserService customOAuth2UserService)
+                        throws Exception {
+                http
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                                .csrf(csrf -> csrf.disable())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/activitats/**").permitAll()
+                                                .requestMatchers("/salas/**").permitAll()
+                                                .requestMatchers("/admin-permitidos/**").authenticated()
+                                                .requestMatchers(HttpMethod.POST, "/usuaris/token").permitAll()
+                                                .requestMatchers("/usuaris/**").authenticated()
+                                                .requestMatchers("/dispositius/**").permitAll()
+                                                .requestMatchers("/roles/**").authenticated()
+                                                .requestMatchers("/recursos/**").authenticated()
+                                                .requestMatchers("/permisos/**").authenticated()
+                                                .requestMatchers("/oauth2/**").permitAll()
+                                                .requestMatchers("/login/**").permitAll()
+                                                .requestMatchers("/swagger-ui/**").permitAll()
+                                                .requestMatchers("/swagger-ui.html").permitAll()
+                                                .requestMatchers("/v3/api-docs/**").permitAll()
+                                                .anyRequest().permitAll())
+                                .oauth2ResourceServer(
+                                                oauth2 -> oauth2.jwt(org.springframework.security.config.Customizer
+                                                                .withDefaults()))
+                                .oauth2Login(oauth -> oauth
+                                                .userInfoEndpoint(userInfo -> userInfo
+                                                                .userService(customOAuth2UserService))
+                                                .defaultSuccessUrl("/oauth/google/home", true)
+                                                .failureUrl("/login.html?error=unauthorized"));
+                return http.build();
+        }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOriginPatterns(List.of("*"));
+                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(List.of("*"));
+                configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 }
